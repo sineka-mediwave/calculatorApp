af67b289f5116ef0e3e8cd9188b42d97d216e286
sineka-mediwave <sineka@mindwaveventures.com>
2023-09-20 15:25:24 +0530

    calculator design

---
+++ app/script.js
@@ -0,0 +1,4 @@
+// program for a simple calculator
+let result = document.querySelector("#result");
+let operand1 = document.querySelectorAll(".value");
+let operator = document.querySelectorAll(".operator");
